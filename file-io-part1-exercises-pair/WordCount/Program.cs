cb61ca66f4100a21cfa17429b5ce5b980eb96eb5
Ted Boliske <tboliske@cinci.rr.com>
2018-06-13 16:49:40 -0400

    paired week 4 updates

--- file-io-part1-exercises-pair/WordCount/Program.cs
+++ file-io-part1-exercises-pair/WordCount/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace WordCount
 {
@@ -10,6 +12,64 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Finding a file and reading the contents.");
+            Console.WriteLine();
+            
+
+            //alice file path = C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part1-exercises-pair
+           // string searchedFile = "";
+            //string parentFilePath = "";
+
+            Console.WriteLine("Please enter the file name and path: ");
+            string parentDirectory = Console.ReadLine();
+            Console.Clear();
+
+            //searchedFile = Console.ReadLine();
+            //string fullPath = Path.Combine(parentFilePath, searchedFile);
+            //string fullPath = parentDirectory + searchedFile;
+            //bool directoryExists = Directory.Exists(parentDirectory);
+            //string parentDirectory = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part1-exercises-pair\alices_adventures_in_wonderland.txt";
+
+            char[] splitChar = { ' ' };
+            char[] sentenceChar = { '.', '!', '?' };
+            //int wordCount = 0;
+            int sentenceCount = 0;
+
+            List<string> AllWords = new List<string>();
+            //List<string> AllSentences = new List<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(parentDirectory))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] words = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        AllWords.AddRange(words);
+
+                        string[] sentences = line.Split(sentenceChar);
+                        sentenceCount += sentences.Length - 1;
+
+                        // Read in the line
+                        //string line = sr.ReadLine().Trim();
+                        //string[] sentences = line.Split(sentenceChar);
+                        //wordCount += line.Split(' ').Count();
+                        //sentenceCount += sentences.Length - 1;
+                    }
+                }
+            }
+            catch (IOException e) //catch a specific type of Exception
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+
+        Console.WriteLine($"There are {AllWords.Count} words in the file.");
+        Console.WriteLine();
+        Console.WriteLine($"There are {sentenceCount} sentences in the file.");
+
+        Console.ReadKey();
         }
     }
 }
--- file-io-part2-exercises-pair/FindAndReplace/Program.cs
+++ file-io-part2-exercises-pair/FindAndReplace/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,98 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
+          
+            Console.WriteLine("Find and replace a word or phrase...");
+            Console.WriteLine();
+            bool exists = false;
+            string sourceFilePath = "";
+            string destinationFilePath = "";
+
+            do
+            {
+                Console.WriteLine("Please enter the file name: ");
+                sourceFilePath = Console.ReadLine();
+                ////string sourceFile = "alices_adventures_in_wonderland.txt";
+
+                if (!Path.IsPathRooted(sourceFilePath))
+                {
+                    sourceFilePath = Path.Combine(Environment.CurrentDirectory, sourceFilePath);
+                }
+
+                if (File.Exists(sourceFilePath))
+                {
+                    exists = true;
+                }
+                else
+                {
+                    Console.WriteLine("File path does not exist.");
+                }
+                if (File.Exists(destinationFilePath))
+                {
+                    return;
+                }
+            } while (!exists);
+
+            Console.WriteLine("Please enter the file path: ");
+            destinationFilePath = Console.ReadLine();
+            destinationFilePath = Path.Combine(Environment.CurrentDirectory, destinationFilePath);
+            //string filePath = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part2-exercises-pair\";
+            //Console.Clear();
+
+            Console.WriteLine("Please enter a search word or phrase: ");
+            string searchString = Console.ReadLine();
+            //string wordUpper = searchString.ToUpper();
+            //string wordLower = searchString.ToLower();
+            Console.Clear();
+
+            Console.WriteLine("Please enter a word or phrase to replace: ");
+            string replaceString = Console.ReadLine();
+            Console.Clear();
+
+            // File path if not hard coded
+            //string parentDirectory = Path.Combine(filePath, sourceFile);
+
+            //hard coded file path
+            //string parentDirectory = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part2-exercises-pair\alices_adventures_in_wonderland.txt";
+
+            //string outputFile = "alices_adventures_in_wonderland-FIXED.txt";
+            //string outputPath = Path.Combine(filePath, outputFile);
+            string fixedLine = "";
+            
+           
+            try
+            {
+                using (StreamReader sr = new StreamReader(sourceFilePath))
+                {
+                    using (StreamWriter sw = new StreamWriter (destinationFilePath, false))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+
+                            //if (line.Contains(searchString) || line.Contains(wordUpper) || line.Contains(wordLower))
+                            //{
+                            //    searchString = searchString.ToLower();
+                            //}
+
+
+                            fixedLine = line.Replace(searchString, replaceString);
+
+                            sw.WriteLine(fixedLine);
+                        }
+                    }
+                       
+                }
+            }
+            catch (IOException e) //catch a specific type of Exception
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Write($"Successfully found and replaced your search term, new file will be called {destinationFilePath}");
+
+            Console.ReadKey();
         }
     }
 }
