d601dc199e43ea3866a1e6bbbbbf28589d5377c9
Ted Boliske <tboliske@cinci.rr.com>
2018-06-13 20:30:59 -0400

    updated week #4 paired exercises

--- file-io-part1-exercises-pair/WordCount/Program.cs
+++ file-io-part1-exercises-pair/WordCount/Program.cs
@@ -12,35 +12,43 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Finding a file and reading the contents.");
-            Console.WriteLine();
-            
-
-            //alice file path = C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part1-exercises-pair
-           // string searchedFile = "";
-            //string parentFilePath = "";
-
-            Console.WriteLine("Please enter the file name and path: ");
-            string parentDirectory = Console.ReadLine();
-            Console.Clear();
-
-            //searchedFile = Console.ReadLine();
-            //string fullPath = Path.Combine(parentFilePath, searchedFile);
-            //string fullPath = parentDirectory + searchedFile;
-            //bool directoryExists = Directory.Exists(parentDirectory);
-            //string parentDirectory = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part1-exercises-pair\alices_adventures_in_wonderland.txt";
+            bool exists = false;
+            string sourceFilePath = "";
 
             char[] splitChar = { ' ' };
             char[] sentenceChar = { '.', '!', '?' };
-            //int wordCount = 0;
             int sentenceCount = 0;
 
             List<string> AllWords = new List<string>();
-            //List<string> AllSentences = new List<string>();
+
+            Console.WriteLine("Finding a file and reading the contents.");
+            Console.WriteLine();
+
+            //string parentDirectory = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part1-exercises-pair\alices_adventures_in_wonderland.txt";
+
+            do
+            {
+                Console.WriteLine("Please enter the file name and path: ");
+                sourceFilePath = Console.ReadLine();
+
+                if (!Path.IsPathRooted(sourceFilePath))
+                {
+                    sourceFilePath = Path.Combine(Environment.CurrentDirectory, sourceFilePath);
+                }
+
+                if (File.Exists(sourceFilePath))
+                {
+                    exists = true;
+                }
+                else
+                {
+                    Console.WriteLine("File path does not exist, please enter a valid path.");
+                }
+            } while (!exists);
 
             try
             {
-                using (StreamReader sr = new StreamReader(parentDirectory))
+                using (StreamReader sr = new StreamReader(sourceFilePath))
                 {
                     while (!sr.EndOfStream)
                     {
@@ -50,12 +58,6 @@ namespace WordCount
 
                         string[] sentences = line.Split(sentenceChar);
                         sentenceCount += sentences.Length - 1;
-
-                        // Read in the line
-                        //string line = sr.ReadLine().Trim();
-                        //string[] sentences = line.Split(sentenceChar);
-                        //wordCount += line.Split(' ').Count();
-                        //sentenceCount += sentences.Length - 1;
                     }
                 }
             }
@@ -65,11 +67,12 @@ namespace WordCount
                 Console.WriteLine(e.Message);
             }
 
-        Console.WriteLine($"There are {AllWords.Count} words in the file.");
-        Console.WriteLine();
-        Console.WriteLine($"There are {sentenceCount} sentences in the file.");
+            Console.WriteLine();
+            Console.WriteLine($"There are {AllWords.Count} words in the file.");
+            Console.WriteLine();
+            Console.WriteLine($"There are {sentenceCount} sentences in the file.");
 
-        Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
