818de37bc6fdad61c44d90afeb1effffc2ef27ba
Ted Boliske <tboliske@cinci.rr.com>
2018-06-13 20:44:28 -0400

    updates to paired exercises

--- file-io-part2-exercises-pair/FindAndReplace/Program.cs
+++ file-io-part2-exercises-pair/FindAndReplace/Program.cs
@@ -11,12 +11,12 @@ namespace FindAndReplace
     {
         static void Main(string[] args)
         {
-          
-            Console.WriteLine("Find and replace a word or phrase...");
-            Console.WriteLine();
             bool exists = false;
             string sourceFilePath = "";
             string destinationFilePath = "";
+
+            Console.WriteLine("Find and replace a word or phrase...");
+            Console.WriteLine();
 
             do
             {
@@ -35,7 +35,7 @@ namespace FindAndReplace
                 }
                 else
                 {
-                    Console.WriteLine("File path does not exist.");
+                    Console.WriteLine("File path does not exist, please enter a valid path.");
                 }
                 if (File.Exists(destinationFilePath))
                 {
@@ -43,30 +43,19 @@ namespace FindAndReplace
                 }
             } while (!exists);
 
-            Console.WriteLine("Please enter the file path: ");
+            Console.WriteLine("Please enter the copy file name: ");
             destinationFilePath = Console.ReadLine();
             destinationFilePath = Path.Combine(Environment.CurrentDirectory, destinationFilePath);
-            //string filePath = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part2-exercises-pair\";
             //Console.Clear();
 
             Console.WriteLine("Please enter a search word or phrase: ");
             string searchString = Console.ReadLine();
-            //string wordUpper = searchString.ToUpper();
-            //string wordLower = searchString.ToLower();
             Console.Clear();
 
             Console.WriteLine("Please enter a word or phrase to replace: ");
             string replaceString = Console.ReadLine();
             Console.Clear();
 
-            // File path if not hard coded
-            //string parentDirectory = Path.Combine(filePath, sourceFile);
-
-            //hard coded file path
-            //string parentDirectory = @"C:\Workspace\Team\team1-c-week4-pair-exercises\file-io-part2-exercises-pair\alices_adventures_in_wonderland.txt";
-
-            //string outputFile = "alices_adventures_in_wonderland-FIXED.txt";
-            //string outputPath = Path.Combine(filePath, outputFile);
             string fixedLine = "";
             
            
@@ -79,12 +68,6 @@ namespace FindAndReplace
                         while (!sr.EndOfStream)
                         {
                             string line = sr.ReadLine();
-
-                            //if (line.Contains(searchString) || line.Contains(wordUpper) || line.Contains(wordLower))
-                            //{
-                            //    searchString = searchString.ToLower();
-                            //}
-
 
                             fixedLine = line.Replace(searchString, replaceString);
 
